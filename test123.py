e71ed518a3acce3f383fe12e18e3ecf09297f1b2
Ganesh V Magham <67683339+riphunter618@users.noreply.github.com>
2026-01-08 10:22:34 +0530

    Replace greet function with weather function

--- test123.py
+++ test123.py
@@ -3,5 +3,6 @@ from fastmcp import FastMCP
 mcp = FastMCP("My MCP Server")
 
 @mcp.tool
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+def weather() -> str:
+    return f"Hello, the termperature is 70 degrees!"
+

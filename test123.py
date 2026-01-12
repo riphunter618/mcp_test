50568c409601fb3db565fd2225c9e182d137f1e8
Ganesh V Magham <67683339+riphunter618@users.noreply.github.com>
2026-01-12 13:31:38 +0530

    Update test123.py

--- test123.py
+++ test123.py
@@ -3,9 +3,16 @@ import requests
 
 mcp = FastMCP("My MCP Server")
 
-@mcp.tool
-def weather(city) -> str:
-    url = f"https://wttr.in/{city}?format=j1"
-    data = requests.get(url).json()
-    return str(data["current_condition"][0]["temp_C"])
 
+@mcp.tool
+def health():
+    url = "https://farmosapi.graylogic.com/health"
+    data = requests.get(url).json()
+    return data
+
+
+@mcp.tool
+def data_types():
+    url = "https://farmosapi.graylogic.com/data-types"
+    data = requests.get(url).json()
+    return data
